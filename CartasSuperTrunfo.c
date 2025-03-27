0f51f7cd7838f53223bbdee5818a60cebef3bf2a
IncompatibleData <samuelcbandeira@gmail.com>
2025-03-27 23:37:39 +0000

    CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,6 @@
 
 int main() {
     printf("Desafio Super Trunfo!\n");
-    printf("CARTA 1\n");
 
     char estado;
     char codigo[10];
@@ -11,6 +10,9 @@ int main() {
     float area;
     float pib;
     int turistico;
+
+
+    printf("CARTA 1\n");
 
     printf("Insira o estado: \n");
     scanf("%c", &estado);
@@ -39,6 +41,41 @@ int main() {
     printf("Insira o número de pontos turísticos: \n");
     scanf("%d", &turistico);
     printf("Número de pontos turísticos: %d\n", turistico);
+    printf("\n");
+
+
+    //INFORMAÇÕES DA CARTA 2//
+
+    printf("CARTA 2\n");
+
+    printf("Insira o estado: \n");
+    scanf(" %[^\n]", &estado);
+    printf("Estado: %c\n", estado);
+
+    printf("Insira o codigo: \n");
+    scanf("%s", codigo);
+    printf("Codigo: %s\n", codigo);
+
+    printf("Insira a cidade: \n");
+    scanf("%s", cidade);
+    printf("Cidade: %s\n", cidade);
+
+    printf("Insira a população: \n");
+    scanf("%d", &populacao);
+    printf("População: %d\n", populacao);
+
+    printf("Insira a área: \n");
+    scanf("%f", &area);
+    printf("Área: %.2f km²\n", area);
+
+    printf("Insira o PIB: \n");
+    scanf("%f", &pib);
+    printf("PIB: %.2f bilhões de reais\n", pib);
+
+    printf("Insira o número de pontos turísticos: \n");
+    scanf("%d", &turistico);
+    printf("Número de pontos turísticos: %d\n", turistico);
+
 
 
 
